52f7dec22f150b1334884e835e1c092aaa778dc0
PuspendraBirajee <119839786+PuspendraBirajee@users.noreply.github.com>
2022-12-21 01:53:23 +0530

    #include<stdio.h> int main() {     int m,n;     scanf("%d%d",&m,&n);     if(m%2==0||n%2==0)     {         printf("Player 1");     }     else     {         printf("Player 2");     } }

---
+++ Predict_the_winner_in_Coin_Game.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int m,n;
+    scanf("%d%d",&m,&n);
+    if(m%2==0||n%2==0)
+    {
+        printf("Player 1");
+    }
+    else
+    {
+        printf("Player 2");
+    }
+}
