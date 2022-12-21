6b8a3f1621e516d4818c8b77ba58e018a1c825f5
PuspendraBirajee <119839786+PuspendraBirajee@users.noreply.github.com>
2022-12-22 01:17:15 +0530

    #include<stdio.h> int fibo(int); int main() {     int n,f,b,d1,d2;     scanf("%d",&n);     for(int i=n;;i++)     {         if(fibo(i))         {             f=i;             break;         }     }     for(int i=n;;i--)     {         if(fibo(i))         {             b=i;             break;         }     }     d1=f-n;     d2=n-b;     if(d1>d2)     {         printf("%d",b);     }     else if(d1<d2)     {         printf("%d",f);     }     else if(d1==d2)     {         printf("%d %d",b,f);     } } int fibo(int n) {     int a=0,b=1,c;     if(n==0||n==1)     {         printf("%d",&n);     }     else     {         a=0;         b=1;         c=a+b;     }     while(n>c)     {         a=b;         b=c;         c=a+b;     }     if(n==c)     {          return 1;     }     else     {         return 0;     } }

---
+++ nearest_fibonacci.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+int fibo(int);
+int main()
+{
+    int n,f,b,d1,d2;
+    scanf("%d",&n);
+    for(int i=n;;i++)
+    {
+        if(fibo(i))
+        {
+            f=i;
+            break;
+        }
+    }
+    for(int i=n;;i--)
+    {
+        if(fibo(i))
+        {
+            b=i;
+            break;
+        }
+    }
+    d1=f-n;
+    d2=n-b;
+    if(d1>d2)
+    {
+        printf("%d",b);
+    }
+    else if(d1<d2)
+    {
+        printf("%d",f);
+    }
+    else if(d1==d2)
+    {
+        printf("%d %d",b,f);
+    }
+}
+int fibo(int n)
+{
+    int a=0,b=1,c;
+    if(n==0||n==1)
+    {
+        printf("%d",&n);
+    }
+    else
+    {
+        a=0;
+        b=1;
+        c=a+b;
+    }
+    while(n>c)
+    {
+        a=b;
+        b=c;
+        c=a+b;
+    }
+    if(n==c)
+    {
+         return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
