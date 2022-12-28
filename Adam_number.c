a901af040141d0b9d54c9e6ef9d13a014d218f93
PuspendraBirajee <119839786+PuspendraBirajee@users.noreply.github.com>
2022-12-28 18:40:04 +0530

    #include<stdio.h> int main() {     int n,r,rev=0,r1,rev1=0;     scanf("%d",&n);     int sq=n*n;
        while(n!=0)
        {
            r=n%10;
            rev=rev*10+r;
            n=n/10;
        }
        int sq1=rev*rev;
        
        while(sq1!=0)
        {
            r1=sq1%10;
            rev1=rev1*10+r1;
            sq1=sq1/10;
        }
        if(sq==rev1)
        {
            printf("True");
        }
        else
        {
            printf("False");
        }
    }

--- Adam_number.c
+++ Adam_number.c
@@ -1,26 +1,30 @@
 #include<stdio.h>
 int main()
 {
-    int n,rev=0,rev1=0;
+    int n,r,rev=0,r1,rev1=0;
     scanf("%d",&n);
-   int sq=n*n;
-   while(n>0)
-   {
-       rev=rev*10+(n%10);
-       n=n/10;
-   }
-   int sqrev=rev*rev;
-   while(sqrev>0)
-   {
-       rev1=rev1*10+(sqrev%10);
-       sqrev=sqrev/10;
-   }
-   if(sq==rev1)
-   {
-       printf("True");
-   }
-   else
-   {
-       printf("False");
-   }
+    int sq=n*n;
+    
+    while(n!=0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    int sq1=rev*rev;
+    
+    while(sq1!=0)
+    {
+        r1=sq1%10;
+        rev1=rev1*10+r1;
+        sq1=sq1/10;
+    }
+    if(sq==rev1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
 }
