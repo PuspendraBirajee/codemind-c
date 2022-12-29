4306bf3574da0f12e82a038810326cb278ba8b8e
PuspendraBirajee <119839786+PuspendraBirajee@users.noreply.github.com>
2022-12-29 10:46:42 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     for(int i=1;i<=t;i++)     {         int x,n;         scanf("%d%d",&x,&n);         printf("%d ",(x*n)/10);     } }

---
+++ Placement_Test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    for(int i=1;i<=t;i++)
+    {
+        int x,n;
+        scanf("%d%d",&x,&n);
+        printf("%d
+",(x*n)/10);
+    }
+}
