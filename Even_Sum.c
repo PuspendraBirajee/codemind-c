86023bf126850eadbaf42dceb2758506ac72a37d
PuspendraBirajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-01-02 21:52:10 +0530

    #include<stdio.h> int main() {     int n,s=0;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(int i=0;i<n;i++)     {         if(a[i]%2==0)         {             s=s+a[i];         }     }     printf("%d",s); }

---
+++ Even_Sum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,s=0;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            s=s+a[i];
+        }
+    }
+    printf("%d",s);
+}
