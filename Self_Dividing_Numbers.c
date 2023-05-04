d4f1855a6b0f9ab5dc2812b79be1e0709f58f148
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-04 22:07:50 +0530

    #include<stdio.h> int self(int n) {     int r,t=n;     while(n!=0){         r=n%10;         if(r!=0){            if(t%r!=0)            {                return 0;            }         }         else{             return 0;         }         n=n/10;     }     return 1; } int main() {  int a,b;  scanf("%d%d",&a,&b);  for(int i=a;i<=b;i++){      if(self(i)){          printf("%d ",i);      }  } }

--- Self_Dividing_Numbers.c
+++ Self_Dividing_Numbers.c
@@ -1,37 +1,29 @@
 #include<stdio.h>
-int pali(int);
+int self(int n)
+{
+    int r,t=n;
+    while(n!=0){
+        r=n%10;
+        if(r!=0){
+           if(t%r!=0)
+           {
+               return 0;
+           }
+        }
+        else{
+            return 0;
+        }
+        n=n/10;
+    }
+    return 1;
+}
 int main()
 {
-    int a,b;
-    scanf("%d%d",&a,&b);
-    for(int i=a;i<=b;i++)
-    {
-        if(pali(i))
-        {
-            printf("%d ",i);
-        }
-    }
-    
-}
-int pali(int n)
-{
-    int r;
-    int t=n;
-    while(n!=0)
-    {
-        r=n%10;
-        if(r!=0)
-        {
-        if(t%r!=0)
-        {
-          return 0; 
-        }
-       }
-      else
-       {
-       return 0;
-      }
-      n=n/10;
-}
-return 1;
+ int a,b;
+ scanf("%d%d",&a,&b);
+ for(int i=a;i<=b;i++){
+     if(self(i)){
+         printf("%d ",i);
+     }
+ }
 }
