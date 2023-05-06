6af638afc5fa55d99485091277270731a78772dd
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-07 00:57:27 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n];     for(int j=0;j<n;j++)     {         scanf("%d",&a[j]);     }     for(int i=n-1;i>0;i--)     {         if(a[i]%2!=0)         {             printf("%d",i);             break;         }
        }
    }

---
+++ last_odd_number_index_position.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int j=0;j<n;j++)
+    {
+        scanf("%d",&a[j]);
+    }
+    for(int i=n-1;i>0;i--)
+    {
+        if(a[i]%2!=0)
+        {
+            printf("%d",i);
+            break;
+        }
+        
+    }
+}
