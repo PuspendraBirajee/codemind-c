614190628a5995b931e8440d1dc956a39c3ac4c5
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-07 01:10:58 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n];     for(int j=0;j<n;j++)     {         scanf("%d",&a[j]);     }     int t,c=0;     scanf("%d",&t);     for(int i=0;i<n;i++)     {         if(a[i]==t)         {           printf("True");           return 0;         }     }
            printf("False");
    }

---
+++ Element_search_in_an_array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int j=0;j<n;j++)
+    {
+        scanf("%d",&a[j]);
+    }
+    int t,c=0;
+    scanf("%d",&t);
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==t)
+        {
+          printf("True");
+          return 0;
+        }
+    }
+  
+        printf("False");
+}
