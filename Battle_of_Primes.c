20c7e2116532d05310c839d78c22a875962c18bb
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-17 13:04:10 +0530

    #include<stdio.h> int prime(int n) {     int c=0;     for(int i=1; i<=n; i++)     {         if(n%i==0)         {             c++;         }     }     if(c==2)     {         return 1;     }     else     {         return 0;     } } int main() {     int n1,n2,f;     scanf("%d %d",&n1,&n2);    int n3=n1+n2;     for(int i=n3+1; ;i++)     {         if(prime(i))         {             f=i;             break;         }     }     int t=f-n3;     printf("%d",t);
    }

--- Battle_of_Primes.c
+++ Battle_of_Primes.c
@@ -1,25 +1,8 @@
 #include<stdio.h>
-int prime(int);
-int main()
-{
-    int n1,n2,f,n3;
-    scanf("%d%d",&n1,&n2);
-    int n=n1+n2;
-    for(int i=n+1;;i++)
-    {
-        if(prime(i))
-        {
-            f=i;
-            break;
-        }
-    }
-    n3=f-n;
-    printf("%d",n3);
-}
 int prime(int n)
 {
     int c=0;
-    for(int i=1;i<=n;i++)
+    for(int i=1; i<=n; i++)
     {
         if(n%i==0)
         {
@@ -30,8 +13,25 @@ int prime(int n)
     {
         return 1;
     }
-    else
+    else 
     {
         return 0;
     }
+}
+int main()
+{
+    int n1,n2,f;
+    scanf("%d %d",&n1,&n2);
+   int n3=n1+n2;
+    for(int i=n3+1; ;i++)
+    {
+        if(prime(i))
+        {
+            f=i;
+            break;
+        }
+    }
+    int t=f-n3;
+    printf("%d",t);
+    
 }
