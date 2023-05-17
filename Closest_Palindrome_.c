cef6a79ef1c5cde6e1e0834a9acdea7320833f2c
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-17 12:47:31 +0530

    #include<stdio.h> int palindrome(int n) {     int p=n;     int rev=0;     while(n!=0)     {         int r=n%10;         rev=rev*10+r;         n=n/10;     }     if(rev==p)     {         return 1;     }     else     {         return 0;     }     }     int main()     {         int n;         scanf("%d",&n);         int f,b;         for(int i=n+1; ; i++)         {             if(palindrome(i))             {                 f=i;                 break;             }         }         for(int i=n-1; ; i--)         {             if(palindrome(i))             {                 b=i;                 break;             }         }         int d1=f-n;         int d2=n-b;         if(d1>d2)         {             printf("%d",b);         }         else if(d2>=d1)         {             printf("%d %d ",b,f);         }
        }

--- Closest_Palindrome_.c
+++ Closest_Palindrome_.c
@@ -1,56 +1,53 @@
 #include<stdio.h>
-int pal(int);
-int main()
+int palindrome(int n)
 {
-    int n,f,b,d1,d2;
-    scanf("%d",&n);
-    for(int i=n+1;;i++)
-    {
-        if(pal(i))
-        {
-            f=i;
-            break;
-        }
-    }
-    for(int i=n-1;;i--)
-    {
-        if(pal(i))
-        {
-            b=i;
-            break;
-        }
-    }
-    d1=f-n;
-    d2=n-b;
-    if(d1>d2)
-    {
-        printf("%d",b);
-    }
-    else if(d1<d2)
-    {
-        printf("%d",f);
-    }
-    else if(d1==d2)
-    {
-        printf("%d %d",b,f);
-    }
-}
-int pal(int n)
-{
-    int r,rev=0;
-    int t=n;
+    int p=n;
+    int rev=0;
     while(n!=0)
     {
-        r=n%10;
+        int r=n%10;
         rev=rev*10+r;
         n=n/10;
     }
-    if(rev==t)
+    if(rev==p)
     {
         return 1;
     }
     else
     {
-        return 0;
+        return 0; 
     }
-}
+    }
+    int main()
+    {
+        int n;
+        scanf("%d",&n);
+        int f,b;
+        for(int i=n+1; ; i++)
+        {
+            if(palindrome(i))
+            {
+                f=i;
+                break; 
+            }
+        }
+        for(int i=n-1; ; i--)
+        {
+            if(palindrome(i)) 
+            {
+                b=i;
+                break;
+            }
+        }
+        int d1=f-n;
+        int d2=n-b;
+        if(d1>d2) 
+        {
+            printf("%d",b);
+        }
+        else if(d2>=d1) 
+        {
+            printf("%d %d ",b,f); 
+        }
+        
+    }
