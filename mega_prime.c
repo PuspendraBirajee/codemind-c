f586ad6ed947d27e38f3204342aaf8abd558a4aa
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-17 12:54:51 +0530

    #include<stdio.h> int mega(int n) {     int c=0;     for(int i=1; i<=n; i++)     {         if(n%i==0)         {             c++;         }     }     if(c==2)     {         return 1;     }     else     {         return 0;     } } int main() {   int n;   scanf("%d",&n);   int c=0;   int count=0; if(mega(n))       {         while(n!=0)         {           int r=n%10;             c++;             if(mega(r))              {                 count++;               }               n=n/10;             }         if(c==count)         {             printf("Mega Prime");         }         else         {         printf("Not Mega Prime");          }       }       else         {             printf("Not Mega Prime");         }     }

--- mega_prime.c
+++ mega_prime.c
@@ -1,51 +1,52 @@
 #include<stdio.h>
-int mega(int);
-int main()
-{
-    int n,r,c1=0,c2=0;
-    scanf("%d",&n);
-   if(mega(n))
-   {
-       while(n!=0)
-    {
-        r=n%10;
-         c1++;
-         if(mega(r))
-         {
-             c2++;
-         }
-         n=n/10;
-    }
-    if(c1==c2)
-    {
-        printf("Mega Prime");
-    }
-    else
-    {
-         printf("Not Mega Prime");
-    }
-   }
-   else
-   {
-       printf("Not Mega Prime");
-   }
-}
 int mega(int n)
 {
     int c=0;
-     for(int i=1;i<=n;i++)
+    for(int i=1; i<=n; i++)
     {
-      if(n%i==0)
-      {
-          c++;
-      }
+        if(n%i==0)
+        {
+            c++;
+        }
     }
     if(c==2)
     {
         return 1;
     }
-    else
+    else 
     {
         return 0;
     }
 }
+int main()
+{
+  int n;
+  scanf("%d",&n);
+  int c=0;
+  int count=0;
+if(mega(n))
+      {
+        while(n!=0)
+        {
+          int r=n%10;
+            c++;
+            if(mega(r))
+             {
+                count++;
+              }
+              n=n/10;
+            }
+        if(c==count)
+        {
+            printf("Mega Prime");
+        }
+        else
+        {
+        printf("Not Mega Prime");
+         }
+      }
+      else 
+        {
+            printf("Not Mega Prime");
+        }
+    }
