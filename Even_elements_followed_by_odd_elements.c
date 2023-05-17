01d21db978dfb143fde7ad36fb44a0b4727349bf
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-17 10:54:20 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }
        for(int j=0;j<n;j++)
        {
           if(a[j]%2==0)
           {
               printf("%d ",a[j]);
           }
        }
         for(int j=0;j<n;j++)
        {
           if(a[j]%2!=0)
           {
               printf("%d ",a[j]);
           }
        }
    }

---
+++ Even_elements_followed_by_odd_elements.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    for(int j=0;j<n;j++)
+    {
+       if(a[j]%2==0)
+       {
+           printf("%d ",a[j]);
+       }
+    }
+     for(int j=0;j<n;j++)
+    {
+       if(a[j]%2!=0)
+       {
+           printf("%d ",a[j]);
+       }
+    }
+}
