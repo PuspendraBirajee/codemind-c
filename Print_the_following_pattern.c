9360bdabc82404bddc489c8b7afe81553601caaf
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-18 00:10:11 +0530

    # include<stdio.h> int main() {      int n;      scanf("%d",&n);      for(int i=n; i>=1; i--)      {          for( int j=1; j<=n; j++)          {              if(i==j || j==n-i+1)              {                  printf("%d ",i);              }              else              {                  printf(" ");              }          }          printf(" ");      } }

--- Print_the_following_pattern.c
+++ Print_the_following_pattern.c
@@ -3,11 +3,18 @@ int main()
 {
      int n;
      scanf("%d",&n);
-     for(int i=1; i<=n; i++)
+     for(int i=n; i>=1; i--)
      {
-         for(int j=i; j<=n; j++)
+         for( int j=1; j<=n; j++)
          {
-             printf("%d ",j);
+             if(i==j || j==n-i+1)
+             {
+                 printf("%d ",i);
+             }
+             else 
+             {
+                 printf(" ");
+             }
          }
          printf("
 ");
