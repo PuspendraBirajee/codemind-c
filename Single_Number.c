5d1fbd79ccad4fe573900a8760a92f37db39c449
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-17 21:49:00 +0530

    # include<stdio.h> int main() {     int n,c=0;     scanf("%d",&n);     int a[n];     for(int i=0; i<n; i++)     {         scanf("%d",&a[i]);     }    int  x=a[0];     for(int i=1; i<n; i++)     {         x=x^a[i];     }     printf("%d",x); }

---
+++ Single_Number.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+int main()
+{
+    int n,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+   int  x=a[0];
+    for(int i=1; i<n; i++)
+    {
+        x=x^a[i];
+    }
+    printf("%d",x);
+}
