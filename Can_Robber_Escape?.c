7e7e9eea862cca67a6074f921324bd7a9d128ffa
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-17 22:07:58 +0530

    # include<stdio.h> int main() {      int n,c=0;      scanf("%d",&n);      int a[n];      for(int i=0; i<n; i++)      {          scanf("%d",&a[i]);      }      for(int i=0; i<n; i++)      {          if(n<=a[i])          {            c++;          }      }      if(c==0)      {          printf("YES");      }      else      {          printf("NO");      } }

---
+++ Can_Robber_Escape?.c
@@ -0,0 +1,26 @@
+# include<stdio.h>
+int main()
+{
+     int n,c=0;
+     scanf("%d",&n);
+     int a[n];
+     for(int i=0; i<n; i++)
+     {
+         scanf("%d",&a[i]);
+     }
+     for(int i=0; i<n; i++)
+     {
+         if(n<=a[i])
+         {
+           c++;  
+         }
+     }
+     if(c==0)
+     {
+         printf("YES");
+     }
+     else 
+     {
+         printf("NO");
+     }
+}
