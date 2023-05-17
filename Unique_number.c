c2d0c396425010d1e08bb42b8080b650fc02f552
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-17 22:50:26 +0530

    # include<stdio.h> int main()     {         int n,r;         scanf("%d",&n);         int x[10];         for(int i=0; i<10; i++)         {             x[i]=0;         }         while(n!=0)         {             r=n%10;             x[r]=x[r]+1;             n=n/10;         }         for(int i=0; i<10; i++)         {           if(x[i]>1)           {               printf("Not Unique Number");                 return 0;           }
            }
            printf("Unique Number");
        }

--- Unique_number.c
+++ Unique_number.c
@@ -1,26 +1,27 @@
 # include<stdio.h>
 int main()
-{
-     int n,r; 
-     scanf("%d",&n);
-     int a[10];
-     for(int i=0; i<10; i++)
-     {
-         a[i]=0;
-     }
-     while(n!=0)
-     {
-         r=n%10;
-         a[r]=a[r]+1;
-         n=n/10;
-     }
-     for(int i=0; i<10; i++)
-     {
-         if(a[i]>1)
-         {
-             printf("Not Unique Number");
-             return 0;
-         }
-     }
-     printf("Unique Number");
-}
+    {
+        int n,r;
+        scanf("%d",&n);
+        int x[10];
+        for(int i=0; i<10; i++)
+        {
+            x[i]=0;
+        }
+        while(n!=0)
+        {
+            r=n%10;
+            x[r]=x[r]+1;
+            n=n/10;
+        }
+        for(int i=0; i<10; i++)
+        {
+          if(x[i]>1)
+          {
+              printf("Not Unique Number");
+                return 0;
+          }
+        
+        }
+        printf("Unique Number");
+    }
