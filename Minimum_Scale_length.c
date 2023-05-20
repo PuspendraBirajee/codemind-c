411632cb854d74d6aa180b02cf58ddcc718b3c8b
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-21 00:51:48 +0530

    # include<stdio.h> int main() {     int n,hcf,i,j;     scanf("%d",&n);     int a[10000];     for(int i=0; i<n; i++)     {         scanf("%d",&a[i]);     }     for(int i=0; i<n; i++)     {    int n1=a[i];    int n2=a[i+1];    for(int j=1; j<=n1 && j<=n2; j++)    {        if(n1%j==0 && n2%j==0)        {            hcf=j;        }    }        a[i+1]=hcf;     }    printf("%d",a[n-1]); }

--- Minimum_Scale_length.c
+++ Minimum_Scale_length.c
@@ -1,24 +1,25 @@
-#include<stdio.h>
+# include<stdio.h>
 int main()
 {
-int arr[100000],i,j,n,n1,n2,hcf;
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-scanf("%d",&arr[i]);
-}
-for(i=0;i<n;i++)
-{
-n1=arr[i];
-n2=arr[i+1];
-for(j=1;j<=n1&&j<=n2;j++)
-{
-if(n1%j==0&&n2%j==0)
-{
-hcf=j;
-}
-}
-arr[i+1]=hcf;
-}
-printf("%d",arr[n-1]);
+    int n,hcf,i,j;
+    scanf("%d",&n);
+    int a[10000];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0; i<n; i++)
+    {
+   int n1=a[i];
+   int n2=a[i+1];
+   for(int j=1; j<=n1 && j<=n2; j++)
+   {
+       if(n1%j==0 && n2%j==0)
+       {
+           hcf=j;
+       }
+   }
+       a[i+1]=hcf;
+    }
+   printf("%d",a[n-1]);
 }
