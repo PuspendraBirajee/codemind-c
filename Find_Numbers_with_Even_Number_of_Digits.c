75bc14fef81a3a9d402968078a667754bb26ba73
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-21 01:50:15 +0530

    # include<stdio.h> int main() {     int n,count=0;     scanf("%d",&n);     int a[n];     for(int i=0;i<n; i++)     {     scanf("%d",&a[i]);     }     for(int i=0; i<n; i++)     {         int c=0;         int t=a[i];         while(t!=0)         {             int r=t%10;             c++;             t=t/10;         }             if(c%2==0)             {                 count++;             }         }     printf("%d",count); }

--- Find_Numbers_with_Even_Number_of_Digits.c
+++ Find_Numbers_with_Even_Number_of_Digits.c
@@ -1,27 +1,27 @@
-#include<stdio.h>
+# include<stdio.h>
 int main()
 {
-    int n;
+    int n,count=0;
     scanf("%d",&n);
     int a[n];
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n; i++)
     {
-        scanf("%d",&a[i]);
+    scanf("%d",&a[i]);
     }
-    int r,c1=0;
-    for(int i=0;i<n;i++)
+    for(int i=0; i<n; i++)
     {
         int c=0;
-        while(a[i]!=0)
+        int t=a[i];
+        while(t!=0)
         {
-        r=a[i]%10;
-        c++;
-        a[i]=a[i]/10; 
+            int r=t%10;
+            c++;
+            t=t/10;
         }
-        if(c%2==0)
-        {
-            c1++;
+            if(c%2==0)
+            {
+                count++;
+            }
         }
-    }
-    printf("%d",c1);
+    printf("%d",count);
 }
