54bacba0379c444c3bb13687cc8818c80136d011
Puspendra Birajee <119839786+PuspendraBirajee@users.noreply.github.com>
2023-05-21 00:49:01 +0530

    #include<stdio.h> int main() {     int n,c=0,p=0;     scanf("%d",&n);     int a[n];     for(int i=0; i<n; i++)     {         scanf("%d",&a[i]);     }     int x=a[0];     for(int i=0; i<n; i++)    {        if(x==a[i])        {            c++;        }    }    if(c==n)    {        printf("0");    }    else    {        for(int i=0; i<n; i++)        {           int count=0;            for(int j=0; j<n; j++)            {                if(a[i]==a[j])                {                    count++;                }            }            if(count>p)            {                p=count;            }        }
       printf("%d",p);
       }
    }

--- Make_Array_Elements_Equal.c
+++ Make_Array_Elements_Equal.c
@@ -1,7 +1,7 @@
-# include<stdio.h>
+#include<stdio.h>
 int main()
 {
-    int n,c=1,p=0;
+    int n,c=0,p=0;
     scanf("%d",&n);
     int a[n];
     for(int i=0; i<n; i++)
@@ -9,35 +9,35 @@ int main()
         scanf("%d",&a[i]);
     }
     int x=a[0];
-    for(int i=1; i<n; i++)
-    {
-        if(x==a[i])
-        {
-            c++;
-        }
-    }
-    if(c==n)
-    {
-        printf("0");
-    }
-    else 
-    {
-    
     for(int i=0; i<n; i++)
-    {
-       int count=0;
-        for(int j=0; j<n; j++)
-        {
-            if(a[i]==a[j])
-            {
-                count++;
-            }
-        }
-        if(count>p)
-        {
-            p=count;
-        }
-    }
-    printf("%d",p);
-}
+   {
+       if(x==a[i])
+       {
+           c++;
+       }
+   }
+   if(c==n)
+   {
+       printf("0");
+   }
+   else 
+   {
+       for(int i=0; i<n; i++)
+       {
+          int count=0;
+           for(int j=0; j<n; j++)
+           {
+               if(a[i]==a[j])
+               {
+                   count++;
+               }
+           }
+           if(count>p)
+           {
+               p=count;
+           }
+       }
+   
+   printf("%d",p);
+   }
 }
